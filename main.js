4b8c2f3dd3230c62fe76b2483058da9643ca4b4e
Diksha Sharma <dikshas948@gmail.com>
2021-06-12 18:15:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var SpeechRecognition= window.webkitSpeechRecognition;
-var recogntiton= new SpeechRecognition();
+var recognition= new SpeechRecognition();
 function start(){
     document.getElementById("textbox").innerHTML="";
-    recogntiton.start();
+    recognition.start();
 }
-recogntiton.onresult=function(event){
+recognition.onresult=function(event){
     console.log(event);
     var Content=event.results[0][0].transcript;
     console.log(Content);
@@ -20,22 +20,23 @@ var synth = window.speechSynthesis;
 speak_data = "taking your selfie in 5 seconds";
 var utterThis = new SpeechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
-Webcam.attach( '#my_camera' );
+Webcam.attach( camera );
 setTimeout(function() {
     take_snapshot();
     save();
 }, 5000);
 }
+camera = document.getElementById("camera");
 Webcam.set({
     width: 320,
     height: 240,
     image_format: 'jpeg',
     jpeg_quality: 90
   });
-camera = document.getElementById("camera");
+
 function take_snapshot(){
 webcam.snap(function(data_url){
-    document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_url+'">';
+    document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_url+'"/>';
 })    
 }
 function save(){
